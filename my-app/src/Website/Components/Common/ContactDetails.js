96fbd69c13545db57c6422e0ab132ca294c7820c
Karthik <karthikdevale@gmail.com>
2024-01-06 23:10:49 +0530

    website : contact page is not completed & photo, other part yet to finish.

--- my-app/src/Website/Components/Common/ContactDetails.js
+++ my-app/src/Website/Components/Common/ContactDetails.js
@@ -61,75 +61,122 @@ const ContactDetails = () => {
 
   return (
     <div className="Contact">
-      <h1>Contact us!</h1>
-      <div class="form-group">
-        <label for="exampleInputFullName">Full Name</label>
-        <input
-          type="text"
-          name="name"
-          class="form-control"
-          aria-describedby="emailHelp"
-          placeholder="Enter Name"
-          value={contactData.name}
-          onChange={(e) => handleContactInput(e)}
-        />
-        {contactError?.name && (
-          <p className="contact-error">*{contactError?.name}</p>
-        )}
-        <label for="exampleInputEmail">Email</label>
-        <input
-          type="email"
-          name="email"
-          class="form-control"
-          placeholder="Enter Email"
-          value={contactData.email}
-          onChange={(e) => handleContactInput(e)}
-        />
-        {contactError?.email && (
-          <p className="contact-error">*{contactError?.email}</p>
-        )}
-        <label for="exampleInputEmail">Country</label>
-        <Select
-          placeholder="Select Country..."
-          isSearchable={true}
-          options={AllCountryOption}
-          onChange={(e) => setContactCountry(e.value)}
-        />
-        {contactError?.country && (
-          <p className="contact-error">*{contactError?.country}</p>
-        )}
-        <label for="exampleInputPhoneNo">Mobile No</label>
-        <input
-          type="number"
-          name="phoneNo"
-          class="form-control"
-          placeholder="Enter Mobile No"
-          value={contactData.phoneNo}
-          onChange={(e) => handleContactInput(e)}
-        />
-        {contactError?.phoneNo && (
-          <p className="contact-error">*{contactError?.phoneNo}</p>
-        )}
-        <label for="exampleInputMessage">Message</label>
-        <textarea
-          class="form-control"
-          placeholder="Message"
-          rows="3"
-        ></textarea>
+      {/* <h1>Contact us!</h1> */}
+
+      <div className="contact-details">
+        <div>
+          <h1>Contact Us</h1>
+          <p>
+            Please fill the form for any Querry Our customer support team will
+            connect with you in 24hrs
+          </p>
+          <div class="form-group">
+            <input
+              type="text"
+              name="name"
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Enter Name"
+              value={contactData.name}
+              onChange={(e) => handleContactInput(e)}
+            />
+            <input
+              type="email"
+              name="email"
+              class="form-control"
+              placeholder="Enter Email"
+              value={contactData.email}
+              onChange={(e) => handleContactInput(e)}
+            />
+            <input
+              type="number"
+              name="phoneNo"
+              class="form-control"
+              placeholder="Enter Mobile No"
+              value={contactData.phoneNo}
+              onChange={(e) => handleContactInput(e)}
+            />
+            <textarea class="form-control" placeholder="Message" rows="3" />
+          </div>
+        </div>
+        <div className="contact-img">
+          <img src="./Contact-Us.jpg" />
+        </div>
+
+        {/* <div className="conatct-form">
+          <h1>Contact us!</h1>
+          <div class="form-group">
+            <label for="exampleInputFullName">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Enter Name"
+              value={contactData.name}
+              onChange={(e) => handleContactInput(e)}
+            />
+            {contactError?.name && (
+              <p className="contact-error">*{contactError?.name}</p>
+            )}
+            <label for="exampleInputEmail">Email</label>
+            <input
+              type="email"
+              name="email"
+              class="form-control"
+              placeholder="Enter Email"
+              value={contactData.email}
+              onChange={(e) => handleContactInput(e)}
+            />
+            {contactError?.email && (
+              <p className="contact-error">*{contactError?.email}</p>
+            )}
+            <label for="exampleInputEmail">Country</label>
+            <Select
+              placeholder="Select Country..."
+              isSearchable={true}
+              options={AllCountryOption}
+              onChange={(e) => setContactCountry(e.value)}
+            />
+            {contactError?.country && (
+              <p className="contact-error">*{contactError?.country}</p>
+            )}
+            <label for="exampleInputPhoneNo">Mobile No</label>
+            <input
+              type="number"
+              name="phoneNo"
+              class="form-control"
+              placeholder="Enter Mobile No"
+              value={contactData.phoneNo}
+              onChange={(e) => handleContactInput(e)}
+            />
+            {contactError?.phoneNo && (
+              <p className="contact-error">*{contactError?.phoneNo}</p>
+            )}
+            <label for="exampleInputMessage">Message</label>
+            <textarea
+              class="form-control"
+              placeholder="Message"
+              rows="3"
+            ></textarea>
+          </div>
+          {contactError ? (
+            <p className="contact-error">Please Enter All Forms</p>
+          ) : (
+            contactFormVarify && (
+              <p className="contact-success">Form is varified</p>
+            )
+          )}
+          <button
+            class={
+              contactError === null ? "btn btn-success" : "btn btn-primary"
+            }
+            onClick={contactDataValidation}
+          >
+            {contactError === null ? "Submit" : "Varify Details"}
+          </button>
+        </div> */}
       </div>
-      {contactError ? (
-        <p className="contact-error">
-          Please Enter All Forms
-        </p>
-      ) : (
-        contactFormVarify && <p className="contact-success">Form is varified</p>
-      )}
-      <button
-        class={contactError === null ? "btn btn-success" : "btn btn-primary"}
-        onClick={contactDataValidation}
-      >
-        {contactError === null ? "Submit" : "Varify Details"}
-      </button>
     </div>
   );
 };
