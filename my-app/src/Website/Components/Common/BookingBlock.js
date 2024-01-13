89d3508d7486f46b4e0952a81484e5b2e6646222
Karthik <karthikdevale@gmail.com>
2024-01-13 20:05:41 +0530

    website : hotels block is cleaned.

--- my-app/src/Website/Components/Common/BookingBlock.js
+++ my-app/src/Website/Components/Common/BookingBlock.js
@@ -5,58 +5,6 @@ import Hotels from "./Hotels";
 const BookingBlock = (props) => {
   const { Data } = props;
 
-  // let revisedData = Data.Destination.places.sort((a, b) => b.rating - a.rating);
-
-  // const [destination, setDestination] = useState([]);
-  // const [isSearchable, setIsSearchable] = useState(true);
-  // const [descType, setDescType] = useState("All");
-  // const [selectedCountry, setSelectedCountry] = useState("All Country");
-  // const [searchBox, setSearchBox] = useState("All Country");
-
-  // const search = () => {
-  //   setSelectedCountry(searchBox);
-  // };
-
-  // const revisedCountry = revisedData.filter(
-  //   (item) => item.country === selectedCountry
-  // );
-
-  // const countries = [...new Set(revisedData.map((item) => item.country))];
-  // countries.unshift("All Country");
-  // countries.sort();
-  // const countryOption = countries.map((item) => ({ label: item, value: item }));
-
-  // const handleNavbar = (type) => {
-  //   const DataType = (revised) =>
-  //     type !== "All" ? revised.filter((item) => item.type === type) : revised;
-
-  //   const revisedNavbar =
-  //     selectedCountry === "All Country"
-  //       ? DataType(revisedData)
-  //       : DataType(revisedCountry);
-  //   setDescType(type);
-  //   setDestination(revisedNavbar);
-  // };
-
-  // const navbar = [
-  //   ...new Set(
-  //     (selectedCountry === "All Country" ? revisedData : revisedCountry).map(
-  //       (item) => item.type
-  //     )
-  //   ),
-  // ];
-  // navbar.unshift("All");
-  // navbar.sort();
-
-  // useEffect(() => {
-  //   setDestination(revisedCountry);
-  //   handleNavbar("All");
-  // }, [selectedCountry]);
-
-  // useEffect(() => {
-  //   setDestination(revisedData);
-  // }, []);
-
   const [bookedDestination, setBookedDestination] = useState([]);
   const [bookedHotel, setBookedHotel] = useState([]);
 
@@ -80,14 +28,17 @@ const BookingBlock = (props) => {
     setBookedDestination: setBookedDestination,
   };
 
+  const hotelsData = {
+    Data: Data,
+    revisedData: revisedData,
+    bookedDestination: bookedDestination,
+    setBookedHotel: setBookedHotel,
+  };
+
   return (
     <div className="booking">
       <Destination {...destinationData} />
-      <Hotels
-        revisedData={revisedData}
-        bookedDestination={bookedDestination}
-        setBookedHotel={setBookedHotel}
-      />
+      <Hotels {...hotelsData} />
     </div>
   );
 };
